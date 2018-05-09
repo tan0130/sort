cbef1e0a94383f750cb6440ac1faaa267b7f2f5f
tan <1311230692@qq.com>
2018-05-09 15:45:29 +0800

    冒泡排序、快速排序、选择排序、插入排序

--- src/sort/Sort_Demo.java
+++ src/sort/Sort_Demo.java
@@ -8,7 +8,18 @@ public class Sort_Demo {
         int[] arr = {1,23,23,45,45,23,456,34};
 
         // 冒泡排序
-        bubbleSort(arr);
+        //bubbleSort(arr);
+
+        // 快速排序
+        /*System.out.println("快速排序后的数组为:");
+        quickSort(0, arr.length - 1, arr);
+        printArr(arr);*/
+
+        // 选择排序
+        //selectSort(arr);
+
+        // 插入排序
+        insertSort(arr);
     }
 
     // 打印数组
@@ -30,6 +41,73 @@ public class Sort_Demo {
             }
         }
         System.out.println("冒泡排序后的数组为:");
+        printArr(arr);
+    }
+
+    /**
+     * 快速排序算法实现
+     * 1.取中值
+     * 2.快速排序算法实现
+     * */
+    public static int getMidValue(int low, int high, int[] arr) {
+        int temp = arr[low]; // 取第一个值为中值
+        while(low < high) {
+            // 先从最左边寻找第一个比基准值小的值，之后进行交换
+            if(low < high && arr[high] >= temp)
+                high--;
+            int temp1 = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+
+            // 再从最右边查找第一个比基准值大的值，之后进行交换
+            if(low < high && arr[low] <= temp)
+                low++;
+            int temp2 = arr[high];
+            arr[high] = arr[low];
+            arr[low] = temp;
+        }
+        return low;
+    }
+    public static void quickSort(int low, int high, int[] arr) {
+        int mid = getMidValue(low, high, arr);
+        if(low < high) {
+            quickSort(low, mid - 1, arr);
+            if(mid != arr.length - 1)
+                quickSort(mid + 1, high, arr);
+        }
+    }
+
+    // 选择排序
+    public static void selectSort(int[] arr) {
+        int min = 0;
+        for (int i = 0; i < arr.length ; i++) {
+            min = i;
+            for (int j = min + 1; j < arr.length; j++) {
+                if(arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            if(min != i) {
+                int temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+        }
+        System.out.println("选择排序后的数组为:");
+        printArr(arr);
+    }
+
+    // 插入排序
+    public static void insertSort(int[] arr) {
+        int currentElem = 0, j = 0;
+        for (int i = 1; i < arr.length; i++) {
+            currentElem = arr[i];
+            for (j = i - 1; j >= 0 && arr[j] > currentElem ; j--) {
+                arr[j + 1] = arr[j];
+            }
+            arr[j + 1] = currentElem;
+        }
+        System.out.println("插入排序后的数组为:");
         printArr(arr);
     }
 }
