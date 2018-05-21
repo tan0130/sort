3e9246ea978a45e82871de01dd790e4d4de4f21d
tan0130 <1311230692@qq.com>
2018-05-21 11:02:47 +0800

    冒泡排序、快速排序、选择排序、插入排序 Demo2

--- src/sort/Sort_Demo2.java
+++ src/sort/Sort_Demo2.java
@@ -1,16 +1,16 @@
 package sort;
 
 /**
- * create by tan on 2018/5/14
- * 数组排序算法：
+ * create by tan on 2018/5/21
+ * 四中基本排序算法实现
  * 1.冒泡排序
  * 2.快速排序
  * 3.选择排序
  * 4.插入排序
  **/
-public class Sort_Demo1 {
+public class Sort_Demo2 {
     public static void main(String[] args) {
-        int[] arr = {1,23,231,1,34,56,345,34};
+        int[] arr = {1, 23, 12, 23, 1,456, 345, 45, 23};
 
         // 冒泡排序
         bubbleSort(arr);
@@ -28,23 +28,24 @@ public class Sort_Demo1 {
 
         // 插入排序
         insertSort(arr);
+
     }
 
-    // 打印数组:遍历数组元素，然后输出控制台
-    private static void printArr(int[] arr) {
+    // 遍历打印数组
+    public static void printArr(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
     }
 
-    // 冒泡排序:通过前后两个元素两两逐次比较，将较大值往后靠
-    private static void bubbleSort(int[] arr) {
+    // 冒泡排序:将前后两个元素逐个比较，将较大值往后靠
+    public static void bubbleSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if(arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
@@ -54,12 +55,12 @@ public class Sort_Demo1 {
 
     /**
      * 快速排序
-     * 1.取中值：第一次选取最左边的元素为中值，先从最右边元素开始，查找第一个小于中值的值，并与中值交换位置，然后再
-     *          从左向右查找第一个大于中值的值，并与中值交换位置，直到low > high，第一次比较结束，之后再重复步骤
-     * 2.快速排序实现：每次将中值前后部分的数组元素进行排序
+     * 1.选取中值，将数组分割成前后两部分
+     * 2.快速排序实现：先从中值最左边开始找到第一个小于中值的数值，与中值交换位置，然后再从最右边开始选取第一个大于
+     * 中值的数，然后交换位置，直到i >= j 时，一趟排序结束
+     *
      * */
-    // 去中值
-    private static int getMid(int low, int high, int[] arr) {
+    public static int getMid(int low, int high, int[] arr) {
         int temp = arr[low];
         while(low < high) {
             if(arr[high] >= temp)
@@ -76,8 +77,7 @@ public class Sort_Demo1 {
         }
         return low;
     }
-    // 快速排序
-    private static void quickSort(int low, int high, int[] arr) {
+    public static void quickSort(int low, int high, int[] arr) {
         int mid = getMid(low, high, arr);
         if(low < high) {
             quickSort(low, mid - 1, arr);
@@ -86,14 +86,15 @@ public class Sort_Demo1 {
         }
     }
 
-    // 选择排序:逐个比较元素，记录较小的那个值，比较结束后将较小值往前放
-    private static void selectSort(int[] arr) {
+    // 选择排序：每次比较选取较小值，将较小值放在数组前面
+    public static void selectSort(int[] arr) {
         int min = 0;
         for (int i = 0; i < arr.length; i++) {
             min = i;
             for (int j = i + 1; j < arr.length; j++) {
-                if(arr[j] < arr[min])
+                if(arr[j] < arr[min]) {
                     min = j;
+                }
             }
             if(min != i) {
                 int temp = arr[i];
@@ -105,8 +106,8 @@ public class Sort_Demo1 {
         printArr(arr);
     }
 
-    // 插入排序：将数组分成一小段，然后后面的元素与currentElem元素比较，如果比currentElem元素大，就交换前后元素位置
-    private static void insertSort(int[] arr) {
+    // 插入排序：数组从左向右选取小区姜，将后面的元素插入到小区间，再将小区间的数值进行排序
+    public static void insertSort(int[] arr) {
         int currentElem = 0, j = 0;
         for (int i = 1; i < arr.length; i++) {
             currentElem = arr[i];
